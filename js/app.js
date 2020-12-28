0258ebdd3784d69a77a86a505c795025747b8e6d
dolphius21 <johnrudolphlee@gmail.com>
2020-12-28 20:50:25 +0800

    make category buttons dynamic

--- js/app.js
+++ js/app.js
@@ -1,43 +1,67 @@
 const menuSection = document.getElementById('menu');
-const filterBtns = document.querySelectorAll('.filter-btn');
+const categoryFilters = document.querySelector('.list-filters');
 
 // Print menu data
 const printHTML = (data) => {
-   let html = data.map(item => {
+   const menuHTML = data.map(obj => {
       return `
          <div class="menu-item">
-            <img src="${item.img}" alt="${item.title}" class="menu-img">
+            <img src="${obj.img}" alt="${obj.title}" class="menu-img">
             <div class="menu-info">
                <div class="menu-title">
-                  <h3 class="menu-name">${item.title}</h3>
-                  <p class="menu-price">$${item.price}</p>
+                  <h3 class="menu-name">${obj.title}</h3>
+                  <p class="menu-price">$${obj.price}</p>
                </div>
                <p class="menu-desc">
-                  ${item.desc}
+                  ${obj.desc}
                </p>
             </div>
          </div>
       `
-   });
-   const print = html.join("");
-   menuSection.innerHTML = print;
+   }).join("");
+   menuSection.innerHTML = menuHTML;
 };
 
+// Display category buttons
+const displayCategoryBtns = (data) => {
+   // get all menu categories
+   const categories = data.reduce((arr, obj) => {
+      if (!arr.includes(obj.category)) {
+         arr.push(obj.category);  
+      }   
+      return arr;
+   }, ['all']);
+   
+   const buttons = categories.map(category => {
+      return `
+         <button class="filter-btn" data-category="${category}">${category}</button>
+      `
+   }).join("");
+   
+   categoryFilters.innerHTML = buttons;
+
+   const filterBtns = categoryFilters.querySelectorAll('.filter-btn');
+
+   // Filter Menu list.
+   filterBtns.forEach(btn => {
+      btn.addEventListener('click', (e) => {
+         console.log(e.currentTarget);
+         const category = e.currentTarget.dataset.category;
+         const menuCategory = menu.filter(menuItem => {
+            // to get the specific category targeted
+            if (menuItem.category === category) {
+               return menuItem;
+            }
+         });
+         // condition if "all" is targeted 
+         (category === 'all') ? printHTML(menu) : printHTML(menuCategory);
+      });
+   });
+}
 
 // Load initial Menu list.
-window.addEventListener('DOMContentLoaded', printHTML(menu));
-
-// Filter Menu list.
-filterBtns.forEach(btn => {
-   btn.addEventListener('click', (e) => {
-      const category = e.currentTarget.dataset.category;
-      const menuCategory = menu.filter(menuItem => {
-         // to get the specific category targeted
-         if (menuItem.category === category) {
-            return menuItem;
-         }
-      });
-      // condition if "all" is targeted 
-      (category === 'all') ? printHTML(menu) : printHTML(menuCategory);
-   });
+window.addEventListener('DOMContentLoaded', () => {
+   displayCategoryBtns(menu);
+   printHTML(menu);
 });
+
